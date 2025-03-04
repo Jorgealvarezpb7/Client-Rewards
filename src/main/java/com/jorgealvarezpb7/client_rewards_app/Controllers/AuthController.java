c83d22401c97d3d122526b61c615f34fd3678fce
Jorge Álvarez <jorgealvarezpb7@gmail.com>
2025-03-05 00:52:35 +0100

    vamos rapido

--- src/main/java/com/jorgealvarezpb7/client_rewards_app/Controllers/AuthController.java
+++ src/main/java/com/jorgealvarezpb7/client_rewards_app/Controllers/AuthController.java
@@ -1,12 +1,14 @@
 package com.jorgealvarezpb7.client_rewards_app.Controllers;
 
+import java.io.IOException;
+
+import com.jorgealvarezpb7.client_rewards_app.App;
 import com.jorgealvarezpb7.client_rewards_app.Services.Database;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 
 public class AuthController {
     private Database db;
@@ -15,14 +17,13 @@ public class AuthController {
         db = new Database();
     }
 
-    //@FXML private Text actiontarget;
     @FXML private TextField usernameField;
     @FXML private PasswordField passwordField;
     
-    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) throws IOException {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
-     //   actiontarget.setText(username + " " + password);
+        App.setRoot("primary");
     }
 }
