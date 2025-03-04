47bb0332a0716d0957c2e3b265f2633986db0864
Jorge Álvarez <jorgealvarezpb7@gmail.com>
2025-03-05 00:36:27 +0100

    concho is in the house

--- src/main/java/com/jorgealvarezpb7/client_rewards_app/App.java
+++ src/main/java/com/jorgealvarezpb7/client_rewards_app/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("auth"), 640, 480);
+        scene = new Scene(loadFXML("Views/Auth/auth"), 640, 480);
         stage.setTitle("Client Rewards");
         stage.setScene(scene);
         stage.setResizable(false);
