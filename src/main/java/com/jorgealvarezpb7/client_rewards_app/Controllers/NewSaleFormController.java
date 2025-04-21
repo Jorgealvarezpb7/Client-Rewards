108c61411ed551f5b3d19e83a795613457580809
Jorge Alvarez <jorgealvarezpb7@gmail.com>
2025-04-21 21:57:54 +0200

    changing textfields in the sales from for comboboxes to avoid user errors

--- src/main/java/com/jorgealvarezpb7/client_rewards_app/Controllers/NewSaleFormController.java
+++ src/main/java/com/jorgealvarezpb7/client_rewards_app/Controllers/NewSaleFormController.java
@@ -1,23 +1,54 @@
 package com.jorgealvarezpb7.client_rewards_app.Controllers;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
+import com.jorgealvarezpb7.client_rewards_app.Models.Client;
+import com.jorgealvarezpb7.client_rewards_app.Models.Product;
 import com.jorgealvarezpb7.client_rewards_app.Services.AppNav;
 import com.jorgealvarezpb7.client_rewards_app.Services.Authenticated;
+import com.jorgealvarezpb7.client_rewards_app.Services.ClientService;
+import com.jorgealvarezpb7.client_rewards_app.Services.ProductService;
 import com.jorgealvarezpb7.client_rewards_app.Utilities.Validator.Validator;
 import com.jorgealvarezpb7.client_rewards_app.Utilities.Validator.ValidatorError;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-public class NewSaleFormController extends AppNav {
+public class NewSaleFormController extends AppNav implements Initializable {
     private Authenticated authenticated;
 
-    @FXML private TextField saleProductIdField;
+    @FXML private ComboBox<String> saleProductIdCbox;
     @FXML private TextField saleQuantityField;
-    @FXML private TextField saleClientIdField;
+    @FXML private ComboBox<String> saleClientIdCbox;
     @FXML private TextField saleTotalAmountField;
+
+    @FXML
+    public void initialize (URL location, ResourceBundle resources) {
+    ProductService productService = new ProductService();
+    ClientService clientService = new ClientService();
+
+
+    ArrayList<Client> clientes = clientService.listClients();
+    if (clientes != null) {
+        for (Client cliente : clientes) {
+            saleClientIdCbox.getItems().add(cliente.getId());
+        }
+    }
+
+    ArrayList<Product> productos = productService.listProducts();
+    if (productos != null) {
+        for (Product producto : productos) {
+            saleProductIdCbox.getItems().add(producto.getId());
+        }
+    }
+}
+
 
     public NewSaleFormController() {
         this.authenticated = Authenticated.getInstance();
@@ -40,9 +71,9 @@ public class NewSaleFormController extends AppNav {
                 globalError.setText("");
                 
 
-                String saleProductId = saleProductIdField.getText();
+                String saleProductId = saleProductIdCbox.getValue();
                 String saleQuantity = saleQuantityField.getText();
-                String saleClientId = saleClientIdField.getText();
+                String saleClientId = saleClientIdCbox.getValue();
                 String saleTotalAmount = saleTotalAmountField.getText();
 
                 Validator productIdValidator = new Validator()
